997a5f4416de5dc8d1124cb999bd5c2ef0126892
Dylan920424 <dylan920424@gmail.com>
2023-02-01 10:37:07 -0600

    phc_morph

--- constants.py
+++ constants.py
@@ -7,8 +7,8 @@ steps = 1000
 maxForce = 500
 sleepTime = 1/48000
 
-numberOfGenerations = 1
-populationSize = 1
+numberOfGenerations = 50
+populationSize = 100
 
 numSensorNeurons = 4
 numMotorNeurons = 8
--- solution.py
+++ solution.py
@@ -9,6 +9,19 @@ class SOLUTION:
     def __init__(self, nextAvailableID) -> None:
         self.myId = nextAvailableID
         self.links = random.randint(2, c.maxLinks)
+        self.sizes = [[random.random(), random.random(), random.random()]]
+        self.connections, self.directions, self.axis, self.posnegs, self.posdif, self.sensorprobs, self.motorprobs, self.boxpos = [], [], [], [], [], [], [], []
+        for i in range(1, self.links):
+            self.connections.append(random.randint(0,i-1))
+            self.sizes.append([random.random(), random.random(), random.random()])
+            self.directions.append(random.randint(0,2))
+            self.axis.append(random.randint(0,2))
+            self.posnegs.append(random.randint(0,1))
+            self.posdif.append([random.random(), random.random(), random.random()])
+            self.boxpos.append([random.random(), random.random(), random.random()])
+            self.sensorprobs.append(random.random() >= 0.2)
+            self.motorprobs.append(random.random() >= 0.2)
+        self.weights = numpy.random.rand(sum(self.sensorprobs),sum(self.motorprobs))*2-1
         self.Create_World()
         self.Create_Body()
         self.Create_Brain()
@@ -36,7 +49,7 @@ class SOLUTION:
 
     def Mutate(self):
         self.Create_World()
-        if random.random() > 0.05 and len(self.have_sensor) > 0 and len(self.have_motors) > 0:
+        if random.random() < 0.6 and len(self.have_sensor) > 0 and len(self.have_motors) > 0:
             if len(self.have_sensor) == 1: 
                 randomRow = 0
             else:
@@ -46,8 +59,29 @@ class SOLUTION:
             else:
                 randomColumn = random.randint(0, len(self.have_motors)-1)
             self.weights[randomRow, randomColumn] = random.random()*2-1
-        # else:
-        #     self.Create_Body()
+        if random.random() < 0.2:
+            randomRow = random.randint(0, len(self.sizes)-1)
+            randomColumn = random.randint(0,2)
+            self.sizes[randomRow][randomColumn] = random.random()
+        if random.random() < 0.2:
+            randomRow = random.randint(0, len(self.posdif)-1)
+            randomColumn = random.randint(0,2)
+            self.posdif[randomRow][randomColumn] = random.random()
+        if random.random() < 0.2:
+            randomRow = random.randint(0, len(self.boxpos)-1)
+            randomColumn = random.randint(0,2)
+            self.boxpos[randomRow][randomColumn] = random.random()
+        if random.random() < 0.2:
+            randomRow = random.randint(0, len(self.connections)-1)
+            if randomRow > 1:
+                self.connections[randomRow] = random.randint(0, randomRow-1)
+        if random.random() < 0.05:
+            randomRow = random.randint(0, len(self.directions)-1)
+            self.directions[randomRow] = random.randint(0,2)
+        if random.random() < 0.05:
+            randomRow = random.randint(0, len(self.axis)-1)
+            self.axis[randomRow] = random.randint(0,2)
+        self.Create_Body()
         self.Create_Brain()
 
     def Create_World(self):
@@ -66,35 +100,31 @@ class SOLUTION:
 
     def Create_Body(self):
         box = [0,0,1]
-        length,width,height = random.random(), random.random(), random.random()
+        length,width,height = self.sizes[0]
         pyrosim.Start_URDF("body" + str(self.myId) + ".urdf")
         pyrosim.Send_Cube(name="link0", pos=box, size=[length,width,height])
         boxes = [box]
-        sizes = [[length,width,height]]
         axises = ["0 0 1", "0 1 0", "1 0 0"]
-        self.have_sensor = []
-        self.have_motors = []
+        self.have_sensor, self.have_motors = [], []
         for i in range(1, self.links):
-            connect = random.randint(0,i-1)
-            direction,axis = random.randint(0,2), random.randint(0,2)
-            posneg = random.randint(0,1)
-            chosen = [-sizes[connect][0]/2, sizes[connect][0]/2, -sizes[connect][1]/2, sizes[connect][1]/2, sizes[connect][2]/2, sizes[connect][2]/2]
-            pos = [boxes[connect][0]+(random.random()-0.5)*sizes[connect][0],boxes[connect][1]+(random.random()-0.5)*sizes[connect][1],boxes[connect][2]+(random.random()-0.5)*sizes[connect][2]]
+            connect = self.connections[i-1]
+            direction,axis = self.directions[i-1], self.axis[i-1]
+            posneg = self.posnegs[i-1]
+            chosen = [-self.sizes[connect][0]/2, self.sizes[connect][0]/2, -self.sizes[connect][1]/2, self.sizes[connect][1]/2, self.sizes[connect][2]/2, self.sizes[connect][2]/2]
+            pos = [boxes[connect][0]+(self.posdif[i-1][0]-0.5)*self.sizes[connect][0],boxes[connect][1]+(self.posdif[i-1][1]-0.5)*self.sizes[connect][1],boxes[connect][2]+(self.posdif[i-1][2]-0.5)*self.sizes[connect][2]]
             pos[direction] = boxes[connect][direction] + chosen[direction*2+posneg]
-            if random.random() > 0.2: self.have_motors.append("link"+str(connect)+"_link"+str(i))
+            if self.motorprobs[i-1]: self.have_motors.append("link"+str(connect)+"_link"+str(i))
             pyrosim.Send_Joint(name = "link"+str(connect)+"_link"+str(i), parent="link"+str(connect), child="link"+str(i), type="revolute", position=pos, jointAxis=axises[axis])
-            length,width,height = random.random(), random.random(), random.random()
-            sizes.append([length,width,height])
-            box = [(random.random()-0.5)*length, (random.random()-0.5)*width, (random.random()-0.5)*height]
+            length,width,height = self.sizes[i]
+            box = [(self.boxpos[i-1][0]-0.5)*length, (self.boxpos[i-1][1]-0.5)*width, (self.boxpos[i-1][2]-0.5)*height]
             chosen = [-length/2, length/2, -width/2, width/2, height/2, height/2]
             box[direction] = chosen[direction*2+posneg]
             boxes.append(box)
-            if random.random() >= 0.2:
+            if self.sensorprobs[i-1]:
                 pyrosim.Send_Cube(name="link"+str(i), pos=box, size=[length,width,height], color="0.0 1.0 0.0 1.0", colorname="Green")
                 self.have_sensor.append(i)
             else:
                 pyrosim.Send_Cube(name="link"+str(i), pos=box, size=[length,width,height])
-        self.weights = numpy.random.rand(len(self.have_sensor),len(self.have_motors))*2-1
         pyrosim.End()
 
     def Create_Brain(self):
